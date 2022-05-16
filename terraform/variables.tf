79809d35abade0220929a5e3d71fb924f6ded49d
yotest1 <100111105+yotest1@users.noreply.github.com>
2022-05-17 00:48:42 +1000

    v

---
+++ terraform/variables.tf
@@ -0,0 +1,19 @@
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "aws_region" {}
+
+variable "admin_ip" {}
+
+variable "availability_zone_a" {}
+variable "availability_zone_b" {}
+
+variable "key_name" {}
+
+variable "database_name" {}
+variable "database_user" {}
+variable "database_password" {}
+
+variable "ssl_certificate_arn" {}
+
+variable "app_name" {}
+variable "app_repo" {}
